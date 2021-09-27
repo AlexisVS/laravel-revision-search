5d851bc1707f6d881b98328dffc86bf0d8a5b0e5
AlexisVS <63750707+AlexisVS@users.noreply.github.com>
2021-09-27 15:54:26 +0200

    base

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,9 @@
+@extends("layout.index")
+@section('content')
+  <div class="w-full flex flex-wrap space-x-10 space-y-10 justify-center">
+    form
+    @foreach ($users as $user)
+        
+    @endforeach
+  </div>
+@endsection
